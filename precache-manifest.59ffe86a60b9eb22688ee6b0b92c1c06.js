b2772ad975b4d47a1f79a082ea0dd5401ea2de4f
Ernesto <ernesto.cubanportrait@gmail.com>
2020-04-27 07:45:35 -0400

    Updates

---
+++ precache-manifest.59ffe86a60b9eb22688ee6b0b92c1c06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28c2432dc2111b2a7770c04f69ad533",
+    "url": "/reactCalculator/index.html"
+  },
+  {
+    "revision": "4b34160fe86e307ccce0",
+    "url": "/reactCalculator/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "d12069edfe241c371315",
+    "url": "/reactCalculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b34160fe86e307ccce0",
+    "url": "/reactCalculator/static/js/2.58594fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCalculator/static/js/2.58594fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12069edfe241c371315",
+    "url": "/reactCalculator/static/js/main.5a2d3aca.chunk.js"
+  },
+  {
+    "revision": "ffe40da783dea7693959",
+    "url": "/reactCalculator/static/js/runtime-main.32fb993e.js"
+  }
+]);
